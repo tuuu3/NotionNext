ba41aefbb38588886f7ed2b03f7fa6dce84686c9
tuuu3 <129585853+tuuu3@users.noreply.github.com>
2023-11-13 16:22:59 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -13,12 +13,12 @@ const BLOG = {
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
   // 3.14.1版本后，欢迎语在此配置，英文逗号隔开 ,  即可支持多个欢迎语打字效果。
-  GREETING_WORDS: process.env.NEXT_PUBLIC_GREETING_WORDS || '徒步爬山、足球、羽毛球球、阅读、game（塞尔达是天😎）',
+  GREETING_WORDS: process.env.NEXT_PUBLIC_GREETING_WORDS || '正经人谁写日记啊',
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '图大猫', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || 'Digital electronic', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || 'Digital electronic，徒步爬山、足球、羽毛球球、阅读、game（塞尔达是天😎）', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://notion-next-delta-sepia.vercel.app/', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
